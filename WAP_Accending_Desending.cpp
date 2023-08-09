54cbe85942cb554b2b3b63d6225ad5d1226e7481
TOPS-JATIN <140719840+TOPS-JATIN@users.noreply.github.com>
2023-08-09 17:14:26 +0530

    Add files via upload

---
+++ WAP_Accending_Desending.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#define input_Space 2
+int main(){
+	int input[input_Space];
+	int Answer[2];
+	int num;
+	int Choice;
+	printf("Give Input \n");
+	for(int i=0;i<input_Space; i++){
+		scanf("%d",&input[i]);
+	};
+	printf("1. Asending\n2. Decending \n");
+	scanf("%d",&Choice);
+	
+	if(Choice == 1 ){
+		(input[0]<input [1]) ? printf("Asending %d\nAsending %d",input[0],input[1]) : printf("Asending %d\nAsending %d",input[1],input[0]);
+	}else{
+		(input[0]>input [1]) ? printf("Decending %d\nDecending %d",input[0],input[1]) : printf("Decending %d\nDecending %d",input[1],input[0]); 
+	}
+	
+
+}
