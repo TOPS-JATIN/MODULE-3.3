91e66bb388ca0e2bb9b6de1be44508622f60f315
TOPS-JATIN <140719840+TOPS-JATIN@users.noreply.github.com>
2023-08-10 15:32:59 +0530

    Add files via upload

--- WAP_2DArrey_Metrix_calculator.cpp
+++ WAP_2DArrey_Metrix_calculator.cpp
@@ -1,11 +1,25 @@
 #include<stdio.h>
 
-#define matrix_ROWS 1
+#define matrix_ROWS 2
 #define matrix_COLS 2
 
 int matrix_1[matrix_ROWS][matrix_COLS];
 int matrix_2[matrix_ROWS][matrix_COLS];
 int metrix[matrix_ROWS][matrix_COLS];
+
+int PRINT(int N){
+	for(int i=0;i<matrix_ROWS;i++){
+		for(int j=0;j<matrix_COLS;j++){
+			
+			if(i==0 && j==0){ printf("metrix = {\n"); };
+			if(i != 0 && j == 0) { printf("\n"); };
+			if(N == 1){	printf("%d ",metrix[i][j]); }
+			else if(N == 2){ printf("%d ",matrix_1[i][j]); }
+			else if(N == 3){ printf("%d ",matrix_2[i][j]); };
+			if(i == matrix_ROWS - 1 && j == matrix_COLS - 1){ printf("\n} \n"); };
+		}
+	}
+}
 
 int ADD(){
 	
@@ -15,9 +29,8 @@ int ADD(){
 			//printf("%d",metrix[i][j]);
 		}
 	};
-	
-	printf("metrix =  {\n %d %d\n}\n",metrix[0][0],metrix[0][1]);
-	
+	PRINT(1);
+
 }
 
 int REMOVE(){
@@ -28,7 +41,7 @@ int REMOVE(){
 		}
 	};
 	
-	printf("metrix =  {\n %d %d\n}\n",metrix[0][0],metrix[0][1]);
+	PRINT(1);
 	
 }
 
@@ -40,7 +53,7 @@ int MULTI(){
 		}
 	};
 	
-	printf("metrix =  {\n %d %d\n}\n",metrix[0][0],metrix[0][1]);
+	PRINT(1);
 	
 }
 
@@ -50,17 +63,22 @@ int main(){
 	
 	for(int i=0;i<matrix_ROWS;i++){
 		for(int j=0;j<matrix_COLS;j++){
-			printf("matrix-1 =  Add %d of %d = ",j,j+1);
+			printf("matrix-1 =  Add %d of %d = ",i,j);
 			scanf("%d",&matrix_1[i][j]);
 		}
-	}
+	};
+	
+    PRINT(2);
+	
 	printf("--------------\n");
 	for(int i=0;i<matrix_ROWS;i++){
 		for(int j=0;j<matrix_COLS;j++){
-			printf("matrix-2 =  Add %d of %d = ",j,j+1);
+			printf("matrix-2 =  Add %d of %d = ",i,j);
 			scanf("%d",&matrix_2[i][j]);
 		}
 	};
+	
+ 	PRINT(3);
 	
 	printf("1. + \n2. - \n3. *\nChoice = ");
 	fflush(stdin);
